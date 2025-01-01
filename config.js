dda4a904a9b1ea924d763166f15d6495422d2af8
X453trail18 <x453trail18@gmail.com>
2025-01-01 09:56:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~zElwHRKT#QV0cx_xPp3kcntbe2xEl-HtMIYWeCN3FYUQs-Ox49i4",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
